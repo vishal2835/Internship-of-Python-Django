e77329d33914c9b831d330f289e2659190e23428
vishal2835 <vishalprajapati2835@gmail.com>
2021-05-27 11:53:08 +0530

    commit

---
+++ Day 2/variables and comments.py
@@ -0,0 +1,24 @@
+x=1
+y= "vishal"
+print(x)
+print(y)
+if 88<89:
+    print(y)
+    print(x)
+    print("value of x is" ,x)
+#this is comment 
+
+
+#this 
+#is 
+#multiline
+#comment
+
+"""this is
+ multiline
+  comment"""
+
+'''this is 
+  also 
+  multiline
+   comment'''
