71a4b631a8d437d38deb356277af00fd717d205e
vishal2835 <vishalprajapati2835@gmail.com>
2021-06-11 07:46:09 +0530

    function based programes

---
+++ Day 5/1.py
@@ -0,0 +1,14 @@
+class MyClass:
+
+    def func1(self):
+        print('have a wonderful Day !')
+
+    def func2(self, name):
+        print("Name is : "+name)
+
+
+myc = MyClass()
+
+
+myc.func1()
+myc.func2("Vishal Prajapati")
