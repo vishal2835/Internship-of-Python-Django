f390836ccdb7c1acdd504a2a0657968e73eab065
vishal2835 <vishalprajapati2835@gmail.com>
2021-06-15 08:06:56 +0530

    commit

---
+++ Day 4/11nestedifelse.py
@@ -0,0 +1,10 @@
+# This time use nested if to solve the problem
+ 
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
